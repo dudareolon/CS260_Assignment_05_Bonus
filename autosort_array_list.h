9830ba42366efb7d64b2fd47a9c1728d9fcfd951
Eduarda (Duda) Reolon <duda.reolon.1589@mail.linnbenton.edu>
2024-02-11 20:38:56 -0800

    list.h file modified to fit array

--- autosort_array_list.h
+++ autosort_array_list.h
@@ -1,16 +1,21 @@
 #pragma once
 
 #include <string>
+#include <vector>
 
+using std::vector;
+using std::string;
 
 // define the list class
 class list {
+    // this a one dimensional array which is a vector
     private:
-        node *head;
+        // according to the internet there are several ways to initialize an empty vector on C++, I am using this:
+        vector<int> array_list;
 
     public:
         list();
         void insert(int value);
-        std::string to_string();
+        string to_string();
         int search(int value);
 };
